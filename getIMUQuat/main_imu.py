b72ef694bdfa40cc29c434fa855f645a08797370
Ricardo Hideki Ito <ric.hideki.ito@gmail.com>
2022-08-24 01:36:29 -0300

    main_imu com 4 imus

--- getIMUQuat/main_imu.py
+++ getIMUQuat/main_imu.py
@@ -20,7 +20,7 @@ imu_configuration = {
     "gyroAutoCalib": True,
     "filterMode": 1,
     "tareSensor": True,
-    "logical_ids": [2, 4, 8],
+    "logical_ids": [ 1, 2, 4, 8],
     "streaming_commands": [0, 255, 255, 255, 255, 255, 255, 255]
 }
 serial_port = serial_op.initialize_imu(imu_configuration)
@@ -29,51 +29,48 @@ while True:
     try:
         print('running...')
         bytes_to_read = serial_port.inWaiting()
-        
-        # NUMERO DO ALEM VAMOS DESCOBRIR PQ - Não tem justificativa ainda.
+
         if  0 < bytes_to_read > 80:
             print("bytes > 0")
             data = serial_port.read(bytes_to_read)
             if data[0] != 0:
                 continue
 
+            if data[1] == 1:
+                extracted_data = serial_op.extract_quaternions(data)
+                quaternions = extracted_data['quaternions']
+                print("IMU1: ", quaternions)
+                sock.SendData( str(1) + ':' + str(quaternions))
+
             if data[1] == 2:
                 extracted_data = serial_op.extract_quaternions(data)
                 quaternions = extracted_data['quaternions']
-#               sock.SendData('Sent from Python: ' + "IMU4 : " + quaternions) # Send this string to other application
-#               sock.SendData( 4 + ',' + quaternions)
-#                data = sock.ReadReceivedData()  # read data
-
                 print("IMU2: ", quaternions)
-#                sock.SendData(str(2) + ',' + str(quaternions))
+                sock.SendData( str(2) + ':' + str(quaternions))
 
             if data[1] == 4:
                 extracted_data = serial_op.extract_quaternions(data)
                 quaternions = extracted_data['quaternions']
-#               sock.SendData('Sent from Python: ' + "IMU4 : " + quaternions) # Send this string to other application
-#               sock.SendData( 4 + ',' + quaternions)
-                data = sock.ReadReceivedData() # read data
-                
                 print("IMU4: ", quaternions)
-            
+                sock.SendData( str(4) + ':' + str(quaternions))
+
             if data[1] == 8:
                 extracted_data = serial_op.extract_quaternions(data)
                 quaternions = extracted_data['quaternions']
-#               sock.SendData('Sent from Python: ' + "IMU8 : " + quaternions) # Send this string to other application
-#               data = sock.ReadReceivedData() # read data
-                
                 print("IMU8: ", quaternions)
                 sock.SendData( str(8) + ':' + str(quaternions))
 
+#               data = sock.ReadReceivedData() # read data
+
     except KeyboardInterrupt:
-        print(GREEN, "Keyboard excpetion occured.", RESET)
+        print("Keyboard excpetion occured.")
         serial_port = serial_op.stop_streaming(serial_port, 
                                                imu_configuration['logical_ids'])
         break
     except Exception:
-        print(RED, "Unexpected exception occured.", RESET)
+        print("Unexpected exception occured.")
         print(traceback.format_exc())
-        print(GREEN, "Stop streaming.", RESET)
+        print("Stop streaming.")
         serial_port = serial_op.stop_streaming(serial_port, 
                                                imu_configuration['logical_ids'])
         break
